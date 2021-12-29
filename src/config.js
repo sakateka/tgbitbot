730439147c6f470d5e156f972c3a5179fa4701cb
Sergey Kacheev <s.kacheev@gmail.com>
2021-12-30 05:33:17 +0700

    fake secrets have more fake

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // secrets
-//export const authToken = "9101153152:AAGhahaha-lolOo_ooolBXqMYj3Uroologl";
-//export const adminChatId = 090111011;
-//export const xPub = 'xpublolooolooolhahadgololhr4LOLaoqJMAs7mQNV8MLPaHB19PXlPMhPP12Hjp32jduEA2rQ93DNYgtzm92ZAUIZKDUAGWNYDXWCMJWNCTPK';
+//export const authToken = "0000000000:XXXXXXXXX-lolOo_ooolXXXXXXXXroologl";
+//export const adminChatId = 000000000;
+//export const xPub = 'xpubxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 
 export const conf = {
     authToken: process.env.AUTH_TOKEN,
